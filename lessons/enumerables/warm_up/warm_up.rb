33bf21da9ae5371dffd45ca6622f259260c26f72
Katya Weicht <katya.weicht@gmail.com>
2025-02-05 10:59:06 -0700

    Work on enumerables practice problems

--- lessons/enumerables/warm_up/warm_up.rb
+++ lessons/enumerables/warm_up/warm_up.rb
@@ -1,10 +1,14 @@
 # 1. Using #each, add to the method below, such that it takes in an array of names, 
 # and returns an array of all names upcased.
 
-kardashians = ["kris", "kim", "kourtney", "khloe", "rob"]
+kardashians = ["kris", "kim", "kourtney", "khloe", "rob"] # global variable
 
-def upcase_names(names)
- # your code here
+def upcase_names(names) #variables inside this fuction are local variables and cannot access global variables
+    upper_kard = []
+ names.each do |name|
+    upper_kard << name.upcase
+ end
+ upper_kard
 end 
 
 p upcase_names(kardashians)
@@ -22,7 +26,13 @@ p upcase_names(kardashians)
 pets = ["Brutus", "Lucky", "Goldie", "Pepper", "Odie"]
 
 def names_ending_in_ie(names)
- # your code here
+    pets_ie = []
+ names.each do |pet_name|
+    if pet_name.end_with?('ie')
+        pets_ie << pet_name
+    end
+ end
+ pets_ie
 end 
 
 p names_ending_in_ie(pets)
@@ -40,6 +50,13 @@ p names_ending_in_ie(pets)
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 def first_num_thats_square_is_above_100(nums)
+    nums.each do |num|
+        num = num * num 
+        if num > 100
+            puts num 
+        end
+
+    end
 
 end 
 
