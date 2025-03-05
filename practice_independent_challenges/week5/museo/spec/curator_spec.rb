60888554f59e34f7085457148ba916f22169018c
Katya Weicht <katya.weicht@gmail.com>
2025-03-04 18:04:21 -0700

    Write method for find_artist_by_id

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -42,5 +42,13 @@ RSpec.describe Curator do
             @curator.add_artist(artist_2)
             expect(@curator.artists).to eq([artist_1,artist_2])
         end
+        it 'can find an artist by the artist id' do
+            artist_1 = Artist.new({id: "1", name: "Henri Cartier-Bresson", born: "1908", died: "2004", country: "France"})  
+            artist_2 = Artist.new({id: "2", name: "Ansel Adams", born: "1902", died: "1984", country: "United States"})  
+            @curator.add_artist(artist_1)
+            @curator.add_artist(artist_2)
+
+            expect(@curator.find_artist_by_id("1")).to eq(artist_1)
+        end
     end
 end
