520875084f2afd0e653a89343353812858f8654d
Katya Weicht <katya.weicht@gmail.com>
2025-03-04 17:57:46 -0700

    Write tests and methods for adding artists and photographs to arrays

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -7,6 +7,40 @@ RSpec.describe Curator do
         @curator = Curator.new
     end
     describe '#initialize' do
-        expect(@curator).to be_a(Curator)
+        it 'exists' do
+            expect(@curator).to be_a(Curator)
+        end
+    end
+    describe "#attributes" do
+        it 'has an array of photographs' do
+            expect(@curator.photographs).to eq([])
+        end
+        it 'has an array of artists' do
+            expect(@curator.artists).to eq([])
+        end
+    end
+    describe "#methods" do
+        it 'can add photographs' do
+            expect(@curator.photographs).to eq([])
+
+            photo_1 = Photograph.new({id: "1", name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: "1", year: "1954"}) 
+            @curator.add_photograph(photo_1)
+            expect(@curator.photographs).to eq([photo_1])
+
+            photo_2 = Photograph.new({id: "2", name: "Moonrise, Hernandez", artist_id: "2", year: "1941"}) 
+            @curator.add_photograph(photo_2)
+            expect(@curator.photographs).to eq([photo_1, photo_2])
+        end
+        it 'can add artists' do
+            expect(@curator.artists).to eq([])
+
+            artist_1 = Artist.new({id: "1", name: "Henri Cartier-Bresson", born: "1908", died: "2004", country: "France"})  
+            @curator.add_artist(artist_1)
+            expect(@curator.artists).to eq([artist_1])
+
+            artist_2 = Artist.new({id: "2", name: "Ansel Adams", born: "1902", died: "1984", country: "United States"})    
+            @curator.add_artist(artist_2)
+            expect(@curator.artists).to eq([artist_1,artist_2])
+        end
     end
 end
